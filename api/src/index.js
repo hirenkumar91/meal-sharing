f5f1d135e4bce9c1c906e1fe981010c9cece3776
hirenkumar91 <hmtrivedi.3@gmail.com>
2024-09-25 07:01:41 +0000

    all-meal route done

--- api/src/index.js
+++ api/src/index.js
@@ -2,32 +2,28 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import knex from "./database_client.js";
+// Import all route//
 import nestedRouter from "./routers/nested.js";
-import path from path;
+import allMealroute from "./routers/all-meal.js";
 
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const apiRouter = express.Router();
-
-// You can delete this route once you add your own routes
-apiRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    process.env.DB_CLIENT === "pg"
-      ? "SELECT * FROM pg_catalog.pg_tables;"
-      : "SHOW TABLES;";
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  res.json({ tables });
+app.get("/", (req, res) => {
+  res.send("Welcome to the Meal Sharing API!");
 });
 
-// This nested router example can also be replaced with your own sub-router
+const apiRouter = express.Router();
+
+// use route
 apiRouter.use("/nested", nestedRouter);
+apiRouter.use("/all-meal", allMealroute);
+app.use("/api",apiRouter);
 
-app.use("/api", apiRouter);
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
