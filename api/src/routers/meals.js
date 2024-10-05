225ad9cb6a4e623b2401dd94118e400c15ce02d8
hirenkumar91 <hmtrivedi.3@gmail.com>
2024-10-05 08:27:50 +0000

    query for meal routs done

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -2,15 +2,85 @@ import express from "express";
 import knex from "../database_client.js";
 
 const mealRouter = express.Router();
-// get all meal
+
 mealRouter.get("/", async (req, res, next) => {
+    try {
+        let query = knex("meal")
+            .leftJoin('Reservation', 'meal.id', 'Reservation.meal_id')
+            .select('meal.*')
+            .groupBy('meal.id')
+            .select(knex.raw('IFNULL(SUM(Reservation.number_of_guests), 0) as total_guests'));
+
+        const {
+            maxPrice,
+            availableReservations,
+            title,
+            dateAfter,
+            dateBefore,
+            limit,
+            sortKey,
+            sortDir,
+        } = req.query;
+
+        if (maxPrice) {
+            const maxPriceNumber = parseFloat(maxPrice);
+            if (!isNaN(maxPriceNumber)) {
+                query.where("meal.price", "<", maxPriceNumber);
+            }
+        }
+
+        if (availableReservations) {
+            const available = availableReservations === 'true';
+            if (available) {
+                query.having('meal.max_reservations', '>', knex.raw('IFNULL(SUM(Reservation.number_of_guests), 0)'));
+            } else {
+                query.having('meal.max_reservations', '<=', knex.raw('IFNULL(SUM(Reservation.number_of_guests), 0)'));
+            }
+        }
+
+        if (title) {
+            query.where("meal.description", "like", `%${title}%`);
+        }
+
+        if (dateAfter) {
+            query.where("meal.when", ">", new Date(dateAfter));
+        }
+
+        if (dateBefore) {
+            query.where("meal.when", "<", new Date(dateBefore));
+        }
+
+        if (limit) {
+            const limitNumber = parseInt(limit);
+            if (!isNaN(limitNumber)) {
+                query.limit(limitNumber);
+            }
+        }
+
+        if (sortKey) {
+            const sortDirValue = sortDir === 'desc' ? 'desc' : 'asc';
+            const validSortKeys = ['when', 'max_reservations', 'price'];
+            if (validSortKeys.includes(sortKey)) {
+                query.orderBy(sortKey, sortDirValue);
+            }
+        }
+
+        const meals = await query;
+        res.json(meals);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// get all meal
+/* mealRouter.get("/", async (req, res, next) => {
     try{
         const all_meals = await knex("meal");
     res.json(all_meals);
     } catch (err) {
         next(err);
       }
-});
+}); */
 
 // add new meal
 
