827afe2b97df94056294398e498b897c9b0cf18b
hirenkumar91 <hmtrivedi.3@gmail.com>
2024-11-27 01:30:56 +0100

    footer done, social media links added and formating done

--- mealapp/components/ui/footer.tsx
+++ mealapp/components/ui/footer.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import styles from "./ui.module.css";
+import { FaFacebook, FaInstagram, FaGoogle } from "react-icons/fa"; // Social media icons
+import { MdCopyright } from "react-icons/md"; // Copyright icon
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div>
-        <h1 >
-          <i>C</i>MealManiac
-        </h1>
+      <div className={styles.copyright}>
+        <MdCopyright size={24} />
+        <h1>MealManiac 2024</h1>
       </div>
-      <div>
-        <i>Facebook</i>
-        <i>Instagram</i>
+      <div className={styles.socialMedia}>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook size={24} />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram size={24} />
+        </a>
+        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+          <FaGoogle size={24} />
+        </a>
       </div>
     </div>
   );
